0cf57845b2aa28dbf4500829c7bee576285621ea
Luiso-o <cheportillo@gmail.com>
2023-11-30 15:00:33 +0100

    test unitarios partidaService

--- src/test/java/Luis/JuegoDados/services/PartidaServiceImplTest.java
+++ src/test/java/Luis/JuegoDados/services/PartidaServiceImplTest.java
@@ -3,6 +3,8 @@ package Luis.JuegoDados.services;
 import Luis.JuegoDados.dto.PartidaDto;
 import Luis.JuegoDados.entity.JugadorEntity;
 import Luis.JuegoDados.entity.PartidaEntity;
+import Luis.JuegoDados.excepciones.EmptyPlayersListException;
+import Luis.JuegoDados.excepciones.PlayerNotFoundException;
 import Luis.JuegoDados.helper.FromEntityToDtoConverter;
 import Luis.JuegoDados.repository.JugadorRepository;
 import Luis.JuegoDados.repository.PartidaRepository;
@@ -59,23 +61,60 @@ class PartidaServiceImplTest {
         assertEquals(partidaDto1.getMensaje(), result.getMensaje());
     }
 
-   /* @Test
+    @Test
     void deleteAllGamesOfPLayerTest(){
-        List<PartidaEntity>myList = new ArrayList<>();
+        List<PartidaEntity> myList = new ArrayList<>();
         myList.add(partida1);
+
         when(jugadorRepository.findById(1L)).thenReturn(Optional.ofNullable(jugador1));
         when(partidaRepository.findByJugador(jugador1)).thenReturn(myList);
-        when(jugadorRepository.save(any(JugadorEntity.class))).thenReturn(new JugadorEntity(1,"Juanito",50));
+        when(jugadorRepository.save(eq(jugador1))).thenReturn(new JugadorEntity(1, "Juanito", 50));
 
         partidaService.deleteAllGamesOfPLayer(1L);
 
-        verify(partidaRepository, times(1)).findByJugador(jugador1);
+        verify(partidaRepository, times(2)).findByJugador(jugador1);
         verify(partidaRepository, times(1)).deleteAll(myList);
-        verify(jugadorRepository, times(1)).save(jugador1);
+        verify(jugadorRepository, times(2)).save(eq(jugador1));
 
         JugadorEntity jugadorDespues = jugadorRepository.findById(1L).orElse(null);
         assertNotNull(jugadorDespues);
         assertEquals(0, jugadorDespues.getPorcentajeExito());
+    }
 
-    }*/
+    @Test
+    void deleteAllGamesOfPLayer_PlayerNotFoundTest() {
+        when(jugadorRepository.findById(1L)).thenReturn(Optional.empty());
+
+        verify(partidaRepository, never()).findByJugador(any());
+        verify(partidaRepository, never()).deleteAll(anyList());
+        verify(jugadorRepository, never()).save(any(JugadorEntity.class));
+
+        assertThrows(PlayerNotFoundException.class, () -> partidaService.deleteAllGamesOfPLayer(1L));
+    }
+
+
+    @Test
+    void findGamesOfAPLayerByIdTest(){
+        List<PartidaEntity> myGames = new ArrayList<>();
+        myGames.add(partida1);
+
+        when(jugadorRepository.findById(1L)).thenReturn(Optional.ofNullable(jugador1));
+        when(partidaRepository.findByJugador(any(JugadorEntity.class))).thenReturn(myGames);
+        when(converter.convertirPartidaEntityADto(any(PartidaEntity.class)))
+                .thenAnswer(invocation -> {
+                    invocation.getArgument(0);
+                    return partidaDto1;
+                });
+
+        List<PartidaDto> result = partidaService.findGamesOfAPLayerById(1L);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+
+        PartidaDto partidaDto = result.get(0);
+        assertEquals(partida1.getId(), partidaDto.getId());
+        assertEquals(partida1.getFecha(), partidaDto.getFecha());
+
+    }
+
 }
